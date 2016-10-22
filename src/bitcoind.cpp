247602cd335701e38ba0100a473d87ee939d3ebc
ftrader <freetrader@tuta.io>
2016-10-22 16:24:11 +0200

    bitcoind client identification (MVHF-CORE-SW-REQ-11-1 / MVHF-CORE-DES-IDME-1)

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
+// Copyright (c) 2016 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -74,7 +75,7 @@ bool AppInit(int argc, char* argv[])
     // Process help and version before taking care about datadir
     if (mapArgs.count("-?") || mapArgs.count("-h") ||  mapArgs.count("-help") || mapArgs.count("-version"))
     {
-        std::string strUsage = _("Bitcoin Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("Bitcoin MVF-Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";  // MVF-Core client name (MVHF-CORE-DES-IDME-1)
 
         if (mapArgs.count("-version"))
         {
@@ -83,7 +84,7 @@ bool AppInit(int argc, char* argv[])
         else
         {
             strUsage += "\n" + _("Usage:") + "\n" +
-                  "  bitcoind [options]                     " + _("Start Bitcoin Core Daemon") + "\n";
+                  "  bitcoind [options]                     " + _("Start Bitcoin MVF-Core Daemon") + "\n";  // MVF-Core client name (MVHF-CORE-DES-IDME-1)
 
             strUsage += "\n" + HelpMessage(HMM_BITCOIND);
         }
