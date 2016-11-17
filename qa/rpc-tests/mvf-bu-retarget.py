39ccde837efe4e2bacd7404dcb659d3648591118
ftrader <freetrader@tuta.io>
2016-11-17 14:30:43 +0100

    align MVF-Core retarget with BU version, temporarily add BU retarget test

--- qa/rpc-tests/mvf-bu-retarget.py
+++ qa/rpc-tests/mvf-bu-retarget.py
@@ -9,15 +9,9 @@
 # on node 0, test pure block height trigger at height 100
 #
 
-import time
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import *
 from random import randint
-from datetime import datetime
-
-BLOCK_SPACING=600               # seconds
-SPACING_WINDOW_TIGHT=100        # +- this many seconds for normal blocks
-SPECIAL_PERIOD=185*144          # ~185 days worth of blocks
 
 class MVF_RETARGET_Test(BitcoinTestFramework):
 
@@ -30,10 +24,18 @@ class MVF_RETARGET_Test(BitcoinTestFramework):
         self.is_network_split = False
         self.nodes.append(start_node(0, self.options.tmpdir,
                             ["-forkheight=100", "-force-retarget" ]))
+        #self.nodes.append(start_node(1, self.options.tmpdir,
+                            #["-forkheight=200", ]))
+        #self.nodes.append(start_node(2, self.options.tmpdir,
+                            #["-forkheight=999999",
+                             #"-blockversion=%s" % 0x20000002])) # signal SegWit
+        #self.nodes.append(start_node(3, self.options.tmpdir,
+                            #["-forkheight=300",
+                             #"-blockversion=%s" % 0x20000002])) # signal SegWit, but forkheight should pre-empt
 
     def is_fork_triggered_on_node(self, node=0):
         """ check in log file if fork has triggered and return true/false """
-        # MVF-Core TODO: extend to check using RPC info about forks
+        # MVF-BU TODO: extend to check using RPC info about forks
         nodelog = self.options.tmpdir + "/node%s/regtest/debug.log" % node
         hf_active = search_file(nodelog, "isMVFHardForkActive=1")
         fork_actions_performed = search_file(nodelog, "MVF: performing fork activation actions")
@@ -57,62 +59,66 @@ class MVF_RETARGET_Test(BitcoinTestFramework):
 
         # use to track how many times the same bits are used in a row
         prev_block = 0
-        count_bits_used = 1
+        count_bits_used = 0
         #diffadjinterval = 0
         prev_block_delta = 0
-        best_block_hash = self.nodes[0].getbestblockhash()
-        last_retarget_block_timestamp = self.nodes[0].getblock(best_block_hash, True)['time']
-        last_wallclock_timestamp = time.time()
 
         # start generating MVF blocks with varying time stamps
-        for n in xrange(SPECIAL_PERIOD):
+        print "nBits changed @ Time,Delta,Block,nBits,Used,Difficulty"
+        for n in xrange(185 * 24 * 60 * 60): #25920
             best_block_hash = self.nodes[0].getbestblockhash()
             best_block = self.nodes[0].getblock(best_block_hash, True)
 
             prev_block = self.nodes[0].getblock(best_block['previousblockhash'], True)
 
-            #if prev_block <> 0 :
+            # don't have this on MVF-Core yet
+            #diffadjinterval = self.nodes[0].getblockchaininfo()['difficultyadjinterval']
+
             if prev_block['bits'] == best_block['bits']:
                 count_bits_used += 1
             else:
-                timenow = time.time()
-                wall_per_block=(timenow - last_wallclock_timestamp) / float(count_bits_used)
-                print "%s %d%+5d %s nBits changed @ Height:nBits:Diff:Used %5d : %s : %0.12f : %4d %4ds/block %0.2fswall/block" %(
-                    datetime.utcnow().isoformat(),
-                    int(timenow),
-                    int(timenow) - int(last_wallclock_timestamp),
+                print "%s,%d,%d,%s,%d,%f " %(
                     time.strftime("%Y-%m-%d %H:%M",time.gmtime(prev_block['time'])),
+                    prev_block_delta,
                     prev_block['height'],
                     prev_block['bits'],
-                    prev_block['difficulty'],
                     count_bits_used,
-                    int((best_block['time'] - last_retarget_block_timestamp) / float(count_bits_used)),
-                    wall_per_block
-                    )
-                    
+                    prev_block['difficulty'])
+
+                #assert_equal(diffadjinterval, count_bits_used)
+
                 count_bits_used = 1
-                last_retarget_block_timestamp = best_block['time']
-                last_wallclock_timestamp = timenow
+            #### end if prev_block['bits'] == best_block['bits']
+
+            # print info for every block
+            #print "%s :: %s :: %f :: %s" %(
+                #best_block['height'],
+                #time.strftime("%H:%M",time.gmtime(best_block['time'])),
+                #best_block['difficulty'],
+                #best_block['bits'])
 
             if n <= 36 :
                 # simulate slow blocks just after the fork i.e. low hash power/high difficulty
-                self.nodes[0].setmocktime(best_block['time'] + randint(4000,6000))
+                next_block_time = randint(4000,6000)
             else:
                 # simulate ontime blocks i.e. hash power/difficult around 600 secs
-                middle=BLOCK_SPACING   # the regular block spacing
-                window_size=SPACING_WINDOW_TIGHT
-                self.nodes[0].setmocktime(best_block['time'] + randint(int(middle - window_size),
-                                                                       int(middle + window_size)))
+                next_block_time = randint(500,700)
+
+            self.nodes[0].setmocktime(best_block['time'] + next_block_time)
+
+            prev_block_delta = best_block['time'] - prev_block['time']
 
             self.nodes[0].generate(1)
 
-            prev_block = best_block
-        #end for n in xrange
+        #### end for n in xrange
+
+        # don't have this on MVF-Core yet
         #diffadjinterval = self.nodes[0].getblockchaininfo()['difficultyadjinterval']
 
-        #print "Final retargeting interval: %s" % diffadjinterval
+        #print diffadjinterval
 
-        print "Done."
+        print "Done. Check the logs now or press enter to shutdown test."
+        raw_input()
 
 if __name__ == '__main__':
     MVF_RETARGET_Test().main()
